016fcae9e6c839625ae96016cbda80c0c97159dc
WMTcore <291458560@qq.com>
2016-03-22 14:47:55 +0800

    Fix 判断内容为空就返回

--- ishadowsocks.js
+++ ishadowsocks.js
@@ -9,10 +9,11 @@ var json = require(path);
 setInterval(function() {
 	request('http://www.ishadowsocks.net', function(error, response, body) {
 		var temp = /(A密码:)[\s\S]+?(<\/h4>)/.exec(body);
+		if (!temp) return;
 		var password = temp[0].replace(temp[1], '').replace(temp[2], '').replace('/\s/g', '');
 		json.configs[0].password = password;
 		json.configs[0].remarks = password;
 		fs.writeFile(path, JSON.stringify(json, null, 2));
-		console.log('当前ishadowsocks 密码为：',password);
+		console.log('当前ishadowsocks 密码为：', password);
 	});
-}, 10*60*1000);
+}, 10 * 60 * 1000);
